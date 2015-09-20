741ed507a8dce38d96dcb6e34acd0a45db721176
nasdev <nealskyer@gmail.com>
2015-09-20 10:13:58 -0700

    First code test
    Added sample .net app

---
+++ git-hello-world-test/git-hello-world-test/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace git_hello_world_test
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdQuit = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.lblMainMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmdQuit
+            // 
+            this.cmdQuit.Location = new System.Drawing.Point(190, 238);
+            this.cmdQuit.Name = "cmdQuit";
+            this.cmdQuit.Size = new System.Drawing.Size(75, 23);
+            this.cmdQuit.TabIndex = 0;
+            this.cmdQuit.Text = "Quit";
+            this.cmdQuit.UseVisualStyleBackColor = true;
+            this.cmdQuit.Click += new System.EventHandler(this.cmdQuit_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(13, 238);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(16, 13);
+            this.lblMessage.TabIndex = 1;
+            this.lblMessage.Text = "...";
+            // 
+            // lblMainMessage
+            // 
+            this.lblMainMessage.AutoSize = true;
+            this.lblMainMessage.Location = new System.Drawing.Point(46, 110);
+            this.lblMainMessage.Name = "lblMainMessage";
+            this.lblMainMessage.Size = new System.Drawing.Size(35, 13);
+            this.lblMainMessage.TabIndex = 2;
+            this.lblMainMessage.Text = "label1";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.lblMainMessage);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.cmdQuit);
+            this.Name = "frmMain";
+            this.Text = "Hello World Git Test";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdQuit;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Label lblMainMessage;
+    }
+}
+
---
+++ git-hello-world-test/git-hello-world-test/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace git_hello_world_test
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        
+
+
+        private void ShowMessage(string message)
+        {
+            try
+            {
+                lblMessage.Text = message;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void cmdQuit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Application.Exit();
+            }
+            catch (Exception ex)
+            {
+                ShowMessage(ex.Message);
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                lblMainMessage.Text = "Hello World";
+            }
+            catch (Exception ex)
+            {
+                ShowMessage(ex.Message);
+            }
+        }
+
+    }
+}
